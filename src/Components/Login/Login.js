b1fdbbeda771f0d7334fc93b471639c425e7a954
parthusun8 <parthusun8@gmail.com>
2022-03-17 11:02:02 +0530

    Initial Commit

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,85 +1,59 @@
 import React from "react";
 import "./Login.css";
-import "animate.css";
-import { useEffect } from "react";
 
 function Login() {
-  // const switchers = [...document.querySelectorAll(".switcher")];
-
-  useEffect(() => {
-    const loginEl = document.querySelector(".login-wrapper");
-    const signupEl = document.querySelector(".signup-wrapper");
-    const login_btn = document.querySelector(".switcher-login");
-    const signup_btn = document.querySelector(".switcher-signup");
-
-    login_btn.addEventListener("click", () => {
-      signupEl.classList.remove("is-active");
-      loginEl.classList.add("is-active");
-    });
-    signup_btn.addEventListener("click", () => {
-      loginEl.classList.remove("is-active");
-      signupEl.classList.add("is-active");
-    });
-  });
-
   return (
-    <section className="forms-section">
-      <div className="forms">
-        <div className="form-wrapper login-wrapper">
-          <button type="button" className="switcher switcher-login">
-            Login
-            <span className="underline" />
-          </button>
-          <form className="form form-login">
-            <fieldset>
-              <legend>Please, enter your email and password for login.</legend>
-              <div className="input-block">
-                <label htmlFor="login-email">E-mail</label>
-                <input id="login-email" type="email" required />
-              </div>
-              <div className="input-block">
-                <label htmlFor="login-password">Password</label>
-                <input id="login-password" type="password" required />
-              </div>
-            </fieldset>
-            <button type="submit" className="btn-login">
-              Login
-            </button>
-          </form>
+    <div className="container">
+      <div className="form-box">
+        <div className="header-form">
+          <h4 className="text-primary text-center">
+            <i className="fa fa-user-circle" style={{ fontSize: "110px" }}></i>
+          </h4>
+          <div className="image"></div>
         </div>
-        <div className="form-wrapper is-active signup-wrapper">
-          <button type="button" className="switcher switcher-signup">
-            Sign Up
-            <span className="underline" />
-          </button>
-          <form className="form form-signup">
-            <fieldset>
-              <legend>
-                Please, enter your email, password and password confirmation for
-                sign up.
-              </legend>
-              <div className="input-block">
-                <label htmlFor="signup-email">E-mail</label>
-                <input id="signup-email" type="email" required />
+        <div className="body-form">
+          <form>
+            <div className="input-group mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text">
+                  <i class="fa fa-user"></i>
+                </span>
               </div>
-              <div className="input-block">
-                <label htmlFor="signup-password">Password</label>
-                <input id="signup-password" type="password" required />
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Username"
+              />
+            </div>
+            <div className="input-group mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text">
+                  <i class="fa fa-lock"></i>
+                </span>
               </div>
-              <div className="input-block">
-                <label htmlFor="signup-password-confirm">
-                  Confirm password
-                </label>
-                <input id="signup-password-confirm" type="password" required />
-              </div>
-            </fieldset>
-            <button type="submit" className="btn-signup">
-              Continue
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Password"
+              />
+            </div>
+            <button type="button" className="btn btn-secondary btn-block">
+              LOGIN
             </button>
+            <div className="message">
+              <div>
+                <a href="#">Forgot your password</a>
+              </div>
+            </div>
           </form>
+          <div className="social">
+            <a href="#">
+              <i className="fab fa-google"></i>
+            </a>
+          </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 }
 
