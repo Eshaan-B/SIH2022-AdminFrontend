3592096df4e214418079bf707257baa029f501d1
parthusun8 <parthusun8@gmail.com>
2022-03-17 09:52:46 +0530

    Dashboard

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Login from "./Components/Login/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./Components/Dashboard/Dashboard";
+import Signup from "./Components/Signup/Signup";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </div>
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,50 +1,85 @@
 import React from "react";
 import "./Login.css";
 import "animate.css";
+import { useEffect } from "react";
 
 function Login() {
+  // const switchers = [...document.querySelectorAll(".switcher")];
+
+  useEffect(() => {
+    const loginEl = document.querySelector(".login-wrapper");
+    const signupEl = document.querySelector(".signup-wrapper");
+    const login_btn = document.querySelector(".switcher-login");
+    const signup_btn = document.querySelector(".switcher-signup");
+
+    login_btn.addEventListener("click", () => {
+      signupEl.classList.remove("is-active");
+      loginEl.classList.add("is-active");
+    });
+    signup_btn.addEventListener("click", () => {
+      loginEl.classList.remove("is-active");
+      signupEl.classList.add("is-active");
+    });
+  });
+
   return (
-    <div className="container animate__animated animate__fadeInRight">
-      <div className="row">
-        <div className="col-lg-3 col-md-2" />
-        <div className="col-lg-6 col-md-8 login-box">
-          <div className="col-lg-12 login-key">
-            <i className="fa fa-key" aria-hidden="true" />
-          </div>
-          <div className="col-lg-12 login-title">ADMIN LOGIN</div>
-          <div className="col-lg-12 login-form">
-            <div className="col-lg-12 login-form">
-              <form autoComplete="off">
-                <div className="form-group">
-                  <label className="form-control-label">EMAIL ID</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    autoComplete="off"
-                  />
-                </div>
-                <div className="form-group">
-                  <label className="form-control-label">PASSWORD</label>
-                  <input type="password" className="form-control" />
-                </div>
-                <div className="col-lg-12 loginbttm">
-                  <div className="col-lg-6 login-btm login-text">
-                    {/* Error Message */}
-                  </div>
-                  <div className="col-lg-6 login-btm login-button">
-                    <button type="submit" className="btn btn-outline-primary">
-                      LOGIN
-                    </button>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-2" />
+    <section className="forms-section">
+      <div className="forms">
+        <div className="form-wrapper login-wrapper">
+          <button type="button" className="switcher switcher-login">
+            Login
+            <span className="underline" />
+          </button>
+          <form className="form form-login">
+            <fieldset>
+              <legend>Please, enter your email and password for login.</legend>
+              <div className="input-block">
+                <label htmlFor="login-email">E-mail</label>
+                <input id="login-email" type="email" required />
+              </div>
+              <div className="input-block">
+                <label htmlFor="login-password">Password</label>
+                <input id="login-password" type="password" required />
+              </div>
+            </fieldset>
+            <button type="submit" className="btn-login">
+              Login
+            </button>
+          </form>
+        </div>
+        <div className="form-wrapper is-active signup-wrapper">
+          <button type="button" className="switcher switcher-signup">
+            Sign Up
+            <span className="underline" />
+          </button>
+          <form className="form form-signup">
+            <fieldset>
+              <legend>
+                Please, enter your email, password and password confirmation for
+                sign up.
+              </legend>
+              <div className="input-block">
+                <label htmlFor="signup-email">E-mail</label>
+                <input id="signup-email" type="email" required />
+              </div>
+              <div className="input-block">
+                <label htmlFor="signup-password">Password</label>
+                <input id="signup-password" type="password" required />
+              </div>
+              <div className="input-block">
+                <label htmlFor="signup-password-confirm">
+                  Confirm password
+                </label>
+                <input id="signup-password-confirm" type="password" required />
+              </div>
+            </fieldset>
+            <button type="submit" className="btn-signup">
+              Continue
+            </button>
+          </form>
         </div>
       </div>
-      <h1 class="animate__animated animate__fadeInLeft">An animated element</h1>
-    </div>
+    </section>
   );
 }
 
